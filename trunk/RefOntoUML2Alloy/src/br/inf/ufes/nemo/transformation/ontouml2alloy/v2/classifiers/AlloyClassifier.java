d97ab8d4b391d5d5c757d18963f553e7ee1acc3b
antognoni.albuquerque@gmail.com <antognoni.albuquerque@gmail.com@b838e576-0234-677d-e459-e360ca8af456>
2011-12-20 02:08:52 +0000

    Pacotes renomeados em conformidade com padrão de rcarraretto

--- trunk/RefOntoUML2Alloy/src/br/inf/ufes/nemo/transformation/ontouml2alloy/v2/classifiers/AlloyClassifier.java
+++
@@ -1,7 +0,0 @@
-package br.inf.ufes.nemo.transformation.ontouml2alloy.v2.classifiers;
-
-public abstract class AlloyClassifier
-{
-	abstract public String getStateName();
-	abstract public String getName();	
-}
