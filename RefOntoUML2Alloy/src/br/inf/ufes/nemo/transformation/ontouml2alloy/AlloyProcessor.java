62074dad9cae9d840196633a4297ba8d45e17cd8
antognoni.albuquerque <antognoni.albuquerque@gmail.com>
2011-12-20 02:08:52 +0000

    Pacotes renomeados em conformidade com padrão de rcarraretto

--- RefOntoUML2Alloy/src/br/inf/ufes/nemo/transformation/ontouml2alloy/AlloyProcessor.java
+++
@@ -1,7 +0,0 @@
-package br.inf.ufes.nemo.transformation.ontouml2alloy;
-
-import RefOntoUML.Model;
-
-public interface AlloyProcessor {
-	String process(Model model);
-}
--- RefOntoUML2Alloy/src/br/inf/ufes/nemo/transformation/ontouml2alloy/v2/classifiers/AlloyIsAnAtom.java
+++
@@ -1,6 +0,0 @@
-package br.inf.ufes.nemo.transformation.ontouml2alloy.v2.classifiers;
-
-public interface AlloyIsAnAtom
-{
-	abstract public String getAtomNames();
-}
