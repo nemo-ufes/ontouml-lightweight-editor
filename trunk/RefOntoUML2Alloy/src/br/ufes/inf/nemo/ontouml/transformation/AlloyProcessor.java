7b86ba71cee2a5bb943e410dce64c9f14a0035a3
antognoni.albuquerque@gmail.com <antognoni.albuquerque@gmail.com@b838e576-0234-677d-e459-e360ca8af456>
2012-01-11 00:09:01 +0000

    Nova implementação, usando os fontes mais recentes de bernardofbbraga

--- trunk/RefOntoUML2Alloy/src/br/ufes/inf/nemo/ontouml/transformation/AlloyProcessor.java
+++
@@ -1,7 +0,0 @@
-package br.ufes.inf.nemo.ontouml.transformation;
-
-import RefOntoUML.Model;
-
-public interface AlloyProcessor {
-	String process(Model model);
-}
--- trunk/RefOntoUML2Alloy/src/br/ufes/inf/nemo/ontouml/transformation/v2/classifiers/AlloyIsAnAtom.java
+++
@@ -1,6 +0,0 @@
-package br.ufes.inf.nemo.ontouml.transformation.v2.classifiers;
-
-public interface AlloyIsAnAtom
-{
-	abstract public String getAtomNames();
-}
