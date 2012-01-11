be1c92b409cb90f9c4ba73c1f2c486441b3dd462
antognoni.albuquerque <antognoni.albuquerque@gmail.com>
2012-01-11 00:09:01 +0000

    Nova implementação, usando os fontes mais recentes de bernardofbbraga

--- RefOntoUML2Alloy/src/br/ufes/inf/nemo/ontouml/transformation/v2/base/AlloyStaticRelationship.java
+++
@@ -1,9 +0,0 @@
-package br.ufes.inf.nemo.ontouml.transformation.v2.base;
-
-import java.io.IOException;
-import java.io.Writer;
-
-public interface AlloyStaticRelationship 
-{
-	void writeTemporalCoExistenceConstraint(Writer out) throws IOException;
-}
--- RefOntoUML2Alloy/src/br/ufes/inf/nemo/ontouml/transformation/v2/classifiers/AlloyClassifier.java
+++
@@ -1,7 +0,0 @@
-package br.ufes.inf.nemo.ontouml.transformation.v2.classifiers;
-
-public abstract class AlloyClassifier
-{
-	abstract public String getStateName();
-	abstract public String getName();	
-}
