2e15a314c63ee28e73c4ef3a58005506be2bb7fd
johnguerson <johnguerson@b838e576-0234-677d-e459-e360ca8af456>
2012-10-01 20:19:58 +0000

    OntoUML2Alloy project: finished the changes on the code.

--- trunk/OntoUML2Alloy/src/br/ufes/inf/nemo/ontouml2alloy/mapper/NamesHandler.java
+++ trunk/OntoUML2Alloy/src/br/ufes/inf/nemo/ontouml2alloy/mapper/NamesHandler.java
@@ -1,5 +1,25 @@
 package br.ufes.inf.nemo.ontouml2alloy.mapper;
 
+/**
+ * Copyright 2011 NEMO (http://nemo.inf.ufes.br/en)
+ *
+ * This file is part of OntoUML2Alloy (OntoUML to Alloy Transformation).
+ *
+ * OntoUML2Alloy is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * OntoUML2Alloy is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with OntoUML2Alloy; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 import java.text.Normalizer;
 import java.util.HashMap;
 
@@ -7,7 +27,9 @@ import java.util.HashMap;
  *	This class is used to remove the special characters of a given name in the model and to
  *	store this modified name into a hash map.
  *
- *  @author John Guerson    
+ * 	@author John Guerson 
+ *  @author Tiago Sales 
+ *  @author Lucas Thom    
  */
 
 public class NamesHandler {
