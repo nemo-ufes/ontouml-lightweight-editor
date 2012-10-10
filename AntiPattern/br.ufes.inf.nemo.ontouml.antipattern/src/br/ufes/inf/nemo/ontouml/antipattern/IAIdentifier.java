075f420087e758f21011318e4076d076ecf300fc
tgoprince <tgoprince@gmail.com>
2012-10-10 14:44:52 +0000

    Imprecise Abstraction Pattern developed.

--- AntiPattern/br.ufes.inf.nemo.ontouml.antipattern/src/br/ufes/inf/nemo/ontouml/antipattern/IAIdentifier.java
+++ AntiPattern/br.ufes.inf.nemo.ontouml.antipattern/src/br/ufes/inf/nemo/ontouml/antipattern/IAIdentifier.java
@@ -11,9 +11,9 @@ import RefOntoUML.Model;
 /*Imprecise Abstraction*/
 public class IAIdentifier {
 	
-	public static String OCLQuery = "Association.allInstances()->select(x:Association | x.memberEnd.type->at(1).oclAsType(Classifier).allChildren()->size()>1 or x.memberEnd.type->at(2).oclAsType(Classifier).allChildren()->size()>1)";
+	public static String OCLQuery = "Association.allInstances()->select(x:Association | (x.memberEnd.type->at(1).oclAsType(Classifier).allChildren()->size()>1 and (x.memberEnd->at(1).upper=-1 or x.memberEnd->at(1).upper>1)) or (x.memberEnd.type->at(2).oclAsType(Classifier).allChildren()->size()>1 and (x.memberEnd->at(2).upper=-1 or x.memberEnd->at(2).upper>1)))";
 	
-	public static Collection<Association> RBOSQuery(Model m) throws ParserException 
+	public static Collection<Association> IAQuery(Model m) throws ParserException 
 	{
 		return (Collection<Association>)OCLQueryExecuter.executeQuery(OCLQuery, m.eClass(), m);
 	}
