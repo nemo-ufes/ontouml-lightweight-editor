d6f777ae15f9f8adb13bc6f1c952aea480b80f29
mauricio0copo@gmail.com <mauricio0copo@gmail.com@b838e576-0234-677d-e459-e360ca8af456>
2013-05-21 14:57:48 +0000

    - added a simple gui; - can now "travel" between worlds; - drill-down yet to be implemented;

---
+++ trunk/br.ufes.inf.nemo.instancevisualizer/src/obj/Skolem.java
@@ -0,0 +1,18 @@
+package obj;
+
+import java.util.ArrayList;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Mauricio
+ */
+public class Skolem extends Field {
+    public Skolem(org.w3c.dom.Element node, ArrayList<Atom> atomList) {
+        super(node, atomList);
+    }
+}
