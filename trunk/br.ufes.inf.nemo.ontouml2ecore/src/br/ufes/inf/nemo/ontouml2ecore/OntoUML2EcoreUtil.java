fc6d633d49e25588a874a567df558bbf212386c6
johnguerson <johnguerson@b838e576-0234-677d-e459-e360ca8af456>
2013-10-28 13:21:09 +0000

    OntoUML2Ecore: I create this transformation because if we have to use the new OCL pivot binding we certainly would need this. In OLED, it is used with Helio's libraries due to a compatibility limitation with the OntoUML Infrastructure, but the transformation can also be used with newest Eclipse releases such as Juno, Kepler, and Luna.

---
+++ trunk/br.ufes.inf.nemo.ontouml2ecore/src/br/ufes/inf/nemo/ontouml2ecore/OntoUML2EcoreUtil.java
@@ -0,0 +1,65 @@
+package br.ufes.inf.nemo.ontouml2ecore;
+
+import java.io.IOException;
+import java.util.Collections;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EPackage;
+import org.eclipse.emf.ecore.EcorePackage;
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
+import org.eclipse.emf.ecore.util.BasicExtendedMetaData;
+import org.eclipse.emf.ecore.util.ExtendedMetaData;
+import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.ecore.xmi.impl.EcoreResourceFactoryImpl;
+
+public class OntoUML2EcoreUtil {
+
+	public static Resource saveEcore (String ecorepath, EPackage ecoremodel) 
+	{
+		ResourceSet ecoreResourceSet = new ResourceSetImpl();
+		URI ecoreURI = URI.createFileURI(ecorepath);
+		
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+		ecoreResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+		ecoreResourceSet.getPackageRegistry().put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
+
+		// enable extended metadata
+		final ExtendedMetaData extendedMetaData = new BasicExtendedMetaData(ecoreResourceSet	.getPackageRegistry());
+		ecoreResourceSet.getLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA,
+		    extendedMetaData);
+				    	
+	    Resource resource = ecoreResourceSet.createResource(ecoreURI);    	
+	    resource.getContents().add(ecoremodel);    	
+	    try{
+	    	resource.save(Collections.emptyMap());
+	    }catch(IOException e){
+	    	e.printStackTrace();
+	    }	    	    
+	    return resource;		   	
+	}
+	
+	public static EPackage readEcore (String ecorePath)
+	{	
+		ResourceSet ecoreResourceSet = new ResourceSetImpl();		
+		URI ecoreURI = URI.createFileURI(ecorePath);
+		
+		Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+		ecoreResourceSet.getResourceFactoryRegistry().getExtensionToFactoryMap().put("ecore", new EcoreResourceFactoryImpl());
+		ecoreResourceSet.getPackageRegistry().put(EcorePackage.eNS_URI, EcorePackage.eINSTANCE);
+				
+		// enable extended metadata
+		final ExtendedMetaData extendedMetaData = new BasicExtendedMetaData(ecoreResourceSet	.getPackageRegistry());
+		ecoreResourceSet.getLoadOptions().put(XMLResource.OPTION_EXTENDED_META_DATA,
+		    extendedMetaData);
+		
+	    Resource ecoreResource = ecoreResourceSet.getResource(ecoreURI,true);
+	    EPackage ecoremodel = (EPackage) ecoreResource.getContents().get(0);
+	    
+	    ecoreResource.getResourceSet().getPackageRegistry().put(ecoremodel.getNsURI(),ecoremodel);
+	    EPackage.Registry.INSTANCE.put(ecoremodel.getNsURI(), ecoremodel);
+	    
+	    return ecoremodel;
+	}	
+}
---
+++ trunk/br.ufes.inf.nemo.ontouml2ecore/src/br/ufes/inf/nemo/ontouml2ecore/tests/OntoUML2EcoreTests.java
@@ -0,0 +1,34 @@
+package br.ufes.inf.nemo.ontouml2ecore.tests;
+
+import java.io.File;
+import java.io.IOException;
+
+import br.ufes.inf.nemo.common.ontoumlparser.OntoUMLParser;
+import br.ufes.inf.nemo.ontouml2ecore.OntoUML2Ecore;
+
+public class OntoUML2EcoreTests {
+	
+	public static void main(String[] args)
+	{
+		try {
+			
+			// we have to use absolute paths. I don't know why.
+						
+			String path1 = new File("src/br/ufes/inf/nemo/ontouml2ecore/tests/models/Imóvel.refontouml").getAbsolutePath();
+			OntoUML2Ecore.convertToEcore(new OntoUMLParser(path1), path1.replace(".refontouml", ".ecore" ), true, true);
+			System.out.println(OntoUML2Ecore.getLog());
+			
+			String path2 = new File("src/br/ufes/inf/nemo/ontouml2ecore/tests/models/project.refontouml").getAbsolutePath();
+			OntoUML2Ecore.convertToEcore(new OntoUMLParser(path2),path2.replace(".refontouml", ".ecore" ), true, true);
+			System.out.println(OntoUML2Ecore.getLog());
+			
+			String path3 = new File("src/br/ufes/inf/nemo/ontouml2ecore/tests/models/RoadTrafficAccident.refontouml").getAbsolutePath();
+			OntoUML2Ecore.convertToEcore(new OntoUMLParser(path3),path3.replace(".refontouml", ".ecore" ), true, true);
+			System.out.println(OntoUML2Ecore.getLog());
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
