ee0150b91eea32a62bfa3fcca828c4c79532a49f
johnguerson <johnguerson@b838e576-0234-677d-e459-e360ca8af456>
2013-12-20 17:47:38 +0000

    OLED & AntiPattern Development: Adding a element (with cloning, and including packages) only to the model, not to diagrams.

--- trunk/br.ufes.inf.nemo.oled/src/br/ufes/inf/nemo/oled/ui/popup/TreePopupMenu.java
+++ trunk/br.ufes.inf.nemo.oled/src/br/ufes/inf/nemo/oled/ui/popup/TreePopupMenu.java
@@ -4,11 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
+import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JPopupMenu;
 import javax.swing.tree.DefaultMutableTreeNode;
 import javax.swing.tree.TreePath;
 
+import br.ufes.inf.nemo.oled.model.ElementType;
 import br.ufes.inf.nemo.oled.ui.AppFrame;
 import br.ufes.inf.nemo.oled.ui.OntoUMLElement;
 import br.ufes.inf.nemo.oled.ui.ProjectBrowser;
@@ -20,9 +22,35 @@ public class TreePopupMenu extends JPopupMenu {
 	private static final long serialVersionUID = 1L;
 		
 	public JMenuItem deleteItem = new JMenuItem("Delete");
-	public JMenuItem autoCompleteItem = new JMenuItem("Complete Selection...");
+	public JMenuItem autoCompleteItem = new JMenuItem("Complete selection");
 	public JMenuItem refreshItem = new JMenuItem("Refresh");
 	public JMenuItem addDiagramItem = new JMenuItem("Add Diagram");
+		
+	public JMenu addMenu = new JMenu("Add");
+	public JMenuItem packageItem = new JMenuItem("Package");
+	public JMenuItem kindItem = new JMenuItem("Kind");
+	public JMenuItem collectiveItem  = new JMenuItem("Collective");
+	public JMenuItem quantityItem = new JMenuItem("Quantity");
+	public JMenuItem subkindItem = new JMenuItem("SubKind");
+	public JMenuItem phaseItem = new JMenuItem("Phase");
+	public JMenuItem roleItem = new JMenuItem("Role");
+	public JMenuItem categoryItem = new JMenuItem("Category");
+	public JMenuItem rolemixinItem = new JMenuItem("RoleMixin");
+	public JMenuItem mixinItem = new JMenuItem("Mixin");	
+	public JMenuItem relatorItem = new JMenuItem("Relator");
+	public JMenuItem modeItem = new JMenuItem("Mode");	
+	public JMenuItem datatypeItem = new JMenuItem("DataType");
+	public JMenuItem genItem = new JMenuItem("Generalization");
+	public JMenuItem mediationItem = new JMenuItem("Mediation");	
+	public JMenuItem materialItem = new JMenuItem("Material");
+	public JMenuItem formalItem = new JMenuItem("Formal");
+	public JMenuItem componentOfItem = new JMenuItem("ComponentOf");
+	public JMenuItem memberOfItem = new JMenuItem("MemberOf");
+	public JMenuItem associationItem = new JMenuItem("Association");
+	public JMenuItem characterizationItem = new JMenuItem("Characterization");
+	public JMenuItem subquantityOfItem = new JMenuItem("SubQuantityOf");
+	public JMenuItem subcollectionOfItem = new JMenuItem("SubCollectionOf");	
+	public JMenuItem derivationItem = new JMenuItem("Derivation");
 	
 	public Object element;
 	
@@ -89,9 +117,107 @@ public class TreePopupMenu extends JPopupMenu {
     				}
     			}
     		});
-    	}   	    	
+    	}  
     	
+    	// Add    	    		
+		if (node.getUserObject() instanceof OntoUMLElement)
+		{
+    		OntoUMLElement ontoElement = ((OntoUMLElement)node.getUserObject());    		
+    		if(ontoElement.getElement() instanceof RefOntoUML.Package)
+    		{
+    			final RefOntoUML.Package eContainer = (RefOntoUML.Package)ontoElement.getElement();
+    			add(addMenu);
+    			addMenu.add(packageItem);
+    	        packageItem.addActionListener(new ActionListener() {				
+    	        	@Override
+    	        	public void actionPerformed(ActionEvent e) {
+    	        		frame.getDiagramManager().add(ElementType.PACKAGE,eContainer);
+    	        	}
+    	        });
+        		addMenu.add(kindItem);
+        		kindItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.KIND,eContainer);
+        			}
+        		});
+        		addMenu.add(quantityItem);
+        		quantityItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.QUANTITY,eContainer);
+        			}
+        		});
+        		addMenu.add(collectiveItem);        
+        		collectiveItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.COLLECTIVE,eContainer);
+        			}
+        		});
+        		addMenu.add(subkindItem);        
+        		subkindItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.SUBKIND,eContainer);
+        			}
+        		});
+        		addMenu.add(phaseItem);
+        		phaseItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.PHASE,eContainer);
+        			}
+        		});
+        		addMenu.add(roleItem);
+        		roleItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.ROLE,eContainer);
+        			}
+        		});
+        		addMenu.add(categoryItem);        
+        		categoryItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.CATEGORY,eContainer);
+        			}
+        		});
+        		addMenu.add(rolemixinItem);
+        		rolemixinItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.ROLEMIXIN,eContainer);
+        			}
+        		});    		
+        		addMenu.add(mixinItem);
+        		mixinItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.MIXIN,eContainer);
+        			}
+        		});  
+        		addMenu.add(modeItem); 
+        		modeItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.MODE,eContainer);
+        			}
+        		});    		
+        		addMenu.add(relatorItem);
+        		relatorItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.RELATOR,eContainer);
+        			}
+        		});    		
+        		addMenu.add(datatypeItem);
+        		datatypeItem.addActionListener(new ActionListener() {				
+        			public void actionPerformed(ActionEvent e) {
+        				frame.getDiagramManager().add(ElementType.DATATYPE,eContainer);
+        			}
+        		});
+        		addMenu.add(genItem);
+        		addMenu.add(materialItem);
+        		addMenu.add(formalItem);
+        		addMenu.add(characterizationItem);
+        		addMenu.add(mediationItem);        
+        		addMenu.add(componentOfItem);        
+        		addMenu.add(memberOfItem);        
+        		addMenu.add(subcollectionOfItem);
+        		addMenu.add(subquantityOfItem);
+        		addMenu.add(associationItem);
+        		addMenu.add(derivationItem);
+    		}
+		}    	
     }
-    
-    
 }
